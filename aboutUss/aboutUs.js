4fce8ff8fef6fcb0dc6c479d8da6ba8e5abbbec8
Yashasvee Dwivedi <yashasveedwivedi69@gmail.com>
2023-05-14 13:43:49 +0530

    Add files via upload

---
+++ aboutUss/aboutUs.js
@@ -0,0 +1,40 @@
+ console.log("yha kuch to h")
+  let so = document.getElementById('infobody');
+  console.log(so);
+  so.style.color = 'red';
+  function chng1(){
+   so.innerHTML = 'You have just clicked on first element';
+  }
+  function chng2(){
+   so.innerHTML = 'You have just clicked on second element';
+  }
+  function chng3(){
+   so.innerHTML = 'You have just clicked on third element';
+  }
+  function chng4(){
+   so.innerHTML = 'You have just clicked on fourth element';
+  }
+  function chng5(){
+   so.innerHTML = 'You have just clicked on fifth element';
+  }
+  function chng6(){
+   so.innerHTML = 'You have just clicked on sixth element';
+  }
+  function chng7(){
+   so.innerHTML = 'You have just clicked on seventh element';
+  }
+  function chng8(){
+   so.innerHTML = 'You have just clicked on eighth element';
+  }
+  function chng9(){
+   so.innerHTML = 'You have just clicked on ninth element';
+  }
+  function chng10(){
+   so.innerHTML = 'You have just clicked on tenth element';
+  }
+  function chng11(){
+   so.innerHTML = 'You have just clicked on eleventh element';
+  }
+  function chng12(){
+   so.innerHTML = 'You have just clicked on twelth element';
+  }
